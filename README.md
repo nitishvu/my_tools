fd84ba5312e05ec5a8d8e597107e5090a3805d0b
Nitish vulli <vullinitish@gmail.com>
2020-05-23 17:42:54 +0530

    ec2 commands

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # my_tools
 scripts i use while working on open source  projects
+
+make sure you have configured aws using `aws configure`
+fill parameters in ec2_devinstance.conf
+
+1)to start ec2 instance
+
+`python3  ec2_devinstance.py start`
+
+2)stop ec2 instance 
+
+`python3  ec2_devinstance.py stop`
+
---
+++ ec2_devinstance.py
@@ -0,0 +1,73 @@
+import boto3
+import configparser
+import sys
+import time
+config = configparser.ConfigParser()
+config.sections()
+config.read('ec2_devinstance.conf')
+region=config['ec2']['region']
+instance=config['ec2']['insance_id']
+ec2 = boto3.client('ec2',region_name=config['ec2']['region'])
+
+def stop_instances(instance_id):
+    try:
+        ec2.stop_instances(InstanceIds=[
+        instance_id,
+    ],)
+    except Exception as e:
+        print(e)
+    print("instance stopped successfully")
+def start_instances(instance_id):
+    try:
+        ec2.start_instances(InstanceIds=[
+        instance_id,
+    ],)
+    except Exception as e:
+        print(e)
+    print("instance started successfully")
+
+
+def wait_for_action_completion(instance):
+    required_state=""
+    if action == 'stop':
+        required_state = 'stopped'
+    if action =='start':
+        required_state = 'running'
+    print("waiting to %s"%action)
+    while (True):
+        response = ec2.describe_instance_status(InstanceIds=[instance],IncludeAllInstances=True)
+        instance_status = response['InstanceStatuses'][0]['InstanceState']['Name']
+        if instance_status==required_state:
+            break
+        else:
+            time.sleep(1)
+    print()
+    print("instance %s completed"%action)
+    
+            
+
+
+
+action = sys.argv[1]
+
+if __name__ == '__main__':
+    #my_region = boto.ec2.connect_to_region(config['ec2']['region'])
+    #reservations = my_region.get_all_instances()
+    response = ec2.describe_instance_status(InstanceIds=[instance],IncludeAllInstances=True)
+    print(response['InstanceStatuses'][0]['InstanceState']['Name'])
+    instance_status = response['InstanceStatuses'][0]['InstanceState']['Name']
+    if action == 'stop':
+        if instance_status=='stopped':
+            print("your instance is already stopped")
+            exit(0)
+        else:
+            stop_instances(instance)
+    if action == 'start':
+        if instance_status=='running':
+            print("your instance is already started")
+            exit(0)
+        else:
+            start_instances(instance)
+    wait_for_action_completion(instance)
+
+    
